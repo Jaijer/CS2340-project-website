9c260598b2e240bf45ac9ae33404102f16dc2529
Jaijer <3lims96@gmail.com>
2024-04-21 17:44:31 -0400

    Introduction added

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,9 +5,11 @@ function Introduction() {
     <div>
       <div className='text-xl font-bold blue_gradient pb-2'>Introduction</div>
       <p>
-      <strong>GreenPlate</strong> is your all-in-one app for sustainable eating. Fight food waste and shop smarter with
-      features like recipe management, pantry tracking, and automatic shopping lists based on recipe needs. GreenPlate empowers
-      you to eat well and live sustainably🌱🍽️.
+        Introducing <strong>GreenPlate</strong>🌱🍽️, your ultimate companion in the quest for sustainable eating habits. With its user-friendly interface and innovative features, GreenPlate revolutionizes the way you approach food consumption. Say goodbye to wasted ingredients and hello to mindful meal planning. <br />
+
+        Effortlessly <span className='blue_gradient'>manage your recipes</span>, <span className='blue_gradient'>keep track of your pantry inventory</span>, and <span className='blue_gradient'>generate shopping lists</span> tailored to your culinary needs—all within a single app. GreenPlate seamlessly integrates these functionalities, streamlining your kitchen experience and empowering you to make environmentally conscious choices every day. <br />
+
+        By reducing food waste and promoting smarter shopping habits, GreenPlate not only enhances your well-being but also contributes to a healthier planet. Join the movement towards sustainable living with GreenPlate—it's time to nourish both yourself and the environment, one plate at a time.
       </p>
     </div>
   )
