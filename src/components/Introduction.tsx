5b80cd69c274da4b4729e5d3ca5454c3ef46a2f3
Jaijer <3lims96@gmail.com>
2024-04-21 18:00:58 -0400

    style adjustment

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,9 +5,9 @@ function Introduction() {
     <div>
       <div className='text-xl font-bold blue_gradient pb-2'>Introduction</div>
       <p>
-        Introducing <strong>GreenPlate</strong>🌱🍽️, your ultimate companion in the quest for sustainable eating habits. With its user-friendly interface and innovative features, GreenPlate revolutionizes the way you approach food consumption. Say goodbye to wasted ingredients and hello to mindful meal planning. <br />
+        Introducing <strong>GreenPlate</strong>🌱🍽️, your ultimate companion in the quest for sustainable eating habits. With its user-friendly interface and innovative features, GreenPlate revolutionizes the way you approach food consumption. Say goodbye to wasted ingredients and hello to mindful meal planning. <br /> <br />
 
-        Effortlessly <span className='blue_gradient'>manage your recipes</span>, <span className='blue_gradient'>keep track of your pantry inventory</span>, and <span className='blue_gradient'>generate shopping lists</span> tailored to your culinary needs—all within a single app. GreenPlate seamlessly integrates these functionalities, streamlining your kitchen experience and empowering you to make environmentally conscious choices every day. <br />
+        Effortlessly <span className='blue_gradient'>manage your recipes</span>, <span className='blue_gradient'>keep track of your pantry inventory</span>, and <span className='blue_gradient'>generate shopping lists</span> tailored to your culinary needs—all within a single app. GreenPlate seamlessly integrates these functionalities, streamlining your kitchen experience and empowering you to make environmentally conscious choices every day. <br /> <br />
 
         By reducing food waste and promoting smarter shopping habits, GreenPlate not only enhances your well-being but also contributes to a healthier planet. Join the movement towards sustainable living with GreenPlate—it is time to nourish both yourself and the environment, one plate at a time.
       </p>
