18b73c330541c473fa9c8c58cc3df5f59728c7d3
Jaijer <3lims96@gmail.com>
2024-04-21 21:20:43 -0400

    conclusion and contributors section added

--- src/components/Conclusion.tsx
+++ src/components/Conclusion.tsx
@@ -4,6 +4,14 @@ function Conclusion() {
   return (
     <div>
       <div className='text-xl font-bold blue_gradient pb-2'>Conclusion</div>
+      <p>
+        Throughout this project, we started from only a concept to develop a functional application over the course of several months. We had to learn and utilize software that was new to most of us - Android Studio and Firebase to create a meal logging app with tracking functionalities. This required us to collaborate, research and ask for help where needed to ensure the success of the team. 
+
+        <br /> <br />We implemented various elements to enhance the application including a pantry, personal cookbook and global cookbook features. We also ensured that user information was linked in the database so that each logged in user would have access to their own saved database of meals. This project combined front-end development with designing and creating user-friendly screens, with the back-end of developing and maintaining a database. For most of us, this was therefore our first foray into full-stack development. 
+
+        <br /> <br />As this was our first time creating such an application, we experienced several challenges along the way. In the initial stages of the project, we had some timeline setbacks due to unforeseen events. We had some difficulty with syncing the database and had to restructure a decent portion of our code to rectify the problems. Having to balance this project alongside other school work and exams required us to strategically assign elements of the project to align with individual schedules to ensure the best result. We began implementing a system that involved assigning specific line items to every team member to make expectations clear.
+
+      </p>
     </div>
   )
 }
--- src/components/Contributors.tsx
+++ src/components/Contributors.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Contributors() {
   return (
     <div>
-      <div className='text-xl font-bold blue_gradient pb-2'>Contributors</div>
+      <div className='text-xl font-bold text-bluish pb-2 text-center'>Contributors</div>
+      <p className='text-bluish text-center pb-3'>
+        Ali Al Sules - Website and Project <br />
+        Shubham Jha - Website and Project <br />
+        Micah Reeves - Website and Project <br />
+        Diego Ruiz - Website and Project <br />
+        Jordanne Stewart - Website and Project
+      </p>
     </div>
   )
 }
