d5904bdeeeedad42b74b572483f77f03e3574db4
Jaijer <3lims96@gmail.com>
2024-04-22 23:09:02 -0400

    styling the video

--- src/components/Functionality.tsx
+++ src/components/Functionality.tsx
@@ -4,7 +4,7 @@ function Functionality() {
   return (
     <div>
       <div className='text-2xl font-bold blue_gradient pb-2'>Functionality</div>
-      <iframe src="https://www.loom.com/embed/84455cf8bee8480281b76e83dd5a41e8?sid=e46f47c4-440c-4fef-a8b6-b5edd815cdb5" height={500} width={750}></iframe>
+      <iframe src="https://www.loom.com/embed/84455cf8bee8480281b76e83dd5a41e8?sid=e46f47c4-440c-4fef-a8b6-b5edd815cdb5" allowFullScreen height={500} width={"100%"}></iframe>
     </div>
   )
 }
